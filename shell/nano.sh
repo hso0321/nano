18a245c361cbe7a09d263017090cea503c2b9c4a
alias0531 <alias0531@kakao.com>
2020-05-13 12:46:40 +0900

    last shell

--- shell/nano.sh
+++ shell/nano.sh
@@ -1,6 +1,12 @@
-#!/bin/bash
 
-rosrun jetbot_ros jetbot_camera& #라즈베리파이 카메라
-roslaunch realsense2_camera rs_t265.launch camera:=cam_2 serial_no:=000094842211071&	#T265
-roslaunch realsense2_camera rs_camera.launch camera:=cam_1 serial_no:=923322071436&	#d435i
-roslaunch turtlebot3_bringup turtlebot3_core.launch&	#turtlebot(opencv보드, 모터)
+#!/bin/bash
+ 
+raspi_cam="rosrun jetbot_ros jetbot_camera"
+t265="roslaunch realsense2_camera rs_t265.launch camera:=cam_2 serial_no:=948422110711"
+d435i="roslaunch realsense2_camera rs_camera.launch camera:=cam_1 serial_no:=923322071436"
+turtlebot="roslaunch turtlebot3_bringup turtlebot3_core.launch"
+
+/bin/bash $raspi_cam&sleep 5&&$t265&sleep 10&&$d435i&sleep 15&&$turtlebot
+
+pkill -9 -ef 'ros'
+exit 0
--- shell/server.sh
+++ shell/server.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-roscore&	#roscore
-rosrun turtlebot3_teleop turtlebot3_teleop_key	#키보드로 조작할때
+roscore&rosrun turtlebot3_teleop turtlebot3_teleop_key;
+
+pkill -9 -ef 'ros'
+
+exit 0
