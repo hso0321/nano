f4072948e39c639dfe6d0a329605e9e81a5f62fe
alias0531 <alias0531@kakao.com>
2020-05-14 12:45:13 +0900

    v2

--- ros_sub/src/ros_sub.cpp
+++ ros_sub/src/ros_sub.cpp
@@ -1,7 +1,13 @@
 #include <ros/ros.h>
+#include <opencv4/opencv2/opencv.hpp>
+#include <image_transport/image_transport.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <vector>
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/Imu.h>
 #include <sensor_msgs/image_encodings.h>
+#include <std_msgs/UInt8MultiArray.h>
 
 void sub_imu_cam_callback(const sensor_msgs::Imu::ConstPtr& msg)
 {
@@ -10,18 +16,49 @@ void sub_imu_cam_callback(const sensor_msgs::Imu::ConstPtr& msg)
 
 void sub_rgb_cam_callback(const sensor_msgs::Image::ConstPtr& msg)
 {
-    ROS_INFO("d435i receive suceess");
+    try
+    {
+        cv_bridge::CvImagePtr cvi;
+        cvi=cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+        cv::imshow("d435i_view", cvi->image);
+        cv::waitKey(1);
+        ROS_INFO("d435i receive suceess");
+    }
+    catch(const std::exception& e)
+    {
+        ROS_ERROR("cannot decode d435i image");
+        std::cerr << e.what() << '\n';
+    }
+    
+
 }
 
 void sub_rasp_cam_callback(const sensor_msgs::Image::ConstPtr& msg)
 {
-    ROS_INFO("jetson_camera receive success");
+    try
+    {
+        cv_bridge::CvImagePtr cvi;
+        cvi=cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+        cv::imshow("rasp_view", cvi->image);
+        cv::waitKey(1);
+        ROS_INFO("rasp_cam receive suceess");
+    }
+    catch(const std::exception& e)
+    {
+        ROS_ERROR("cannot decode d435i image");
+        std::cerr << e.what() << '\n';
+    }
 }
 
 int main(int argc, char **argv)
 {
     //초기설정
     ros::init(argc, argv, "ros_subscribe");
+
+    cv::namedWindow("d435i_view");
+    cv::namedWindow("rasp_view");
+    cv::startWindowThread();
+
     //노드 제어
     ros::NodeHandle n;
     ros::Subscriber sub_rasp_cam = n.subscribe("/jetbot_camera/raw", 2, sub_rasp_cam_callback);
@@ -29,6 +66,8 @@ int main(int argc, char **argv)
     ros::Subscriber sub_imu_cam = n.subscribe("/imu", 2, sub_imu_cam_callback);
 
     ros::spin();
+
+    cv::destroyAllWindows();
 
     return 0;
 }
---
+++ start_shell/ros_sub_recieve.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rosrun ros_sub ros_sub_node 
+
+exit 0
